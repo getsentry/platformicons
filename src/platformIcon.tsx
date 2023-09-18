b2f0b85c6298209a3f1fe599062dae988e89ca6d
Daniel Griesser <daniel.griesser.86@gmail.com>
2023-09-18 08:38:01 +0200

    feat: Add bun logo

--- src/platformIcon.tsx
+++ src/platformIcon.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export const PLATFORM_TO_ICON = {
   android: "android",
   apple: "apple",
+  bun: "bun",
   capacitor: "capacitor",
   clojure: "clojure",
   cocoa: "apple",
